5b3d9878028c26af48fcb86f536f2f19f212bebc
adnan-zahir <74122975+adnan-zahir@users.noreply.github.com>
2024-07-14 17:53:40 +0700

    Added: Kegiatan and Gallery sections to home page Note: Data still static

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Link } from "@nextui-org/link";
 import { button as buttonStyles } from "@nextui-org/theme";
+import { Card, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
@@ -7,37 +9,114 @@ import { InstagramIcon } from "@/components/icons";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h2 className={subtitle()}>Terbaru</h2>
-        <h1 className={title()}>Pengembaraan</h1>
-        <br />
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Lihat serunya ka;mi mengembara gunung dan hutan untuk eksplorasi
-          berbagai macam flora dan fauna!
-        </h2>
-      </div>
+    <>
+      {/* HERO::START */}
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h2 className={subtitle()}>Terbaru</h2>
+          <h1 className={title()}>Pengembaraan</h1>
+          <br />
+          <h2 className={subtitle({ class: "mt-4" })}>
+            Lihat serunya kami mengembara gunung dan hutan untuk eksplorasi
+            berbagai macam flora dan fauna!
+          </h2>
+        </div>
 
-      <div className="flex gap-3">
-        <Link
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href="#"
-        >
-          Lihat
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.instagram}
-        >
-          <InstagramIcon size={20} />
-          Instagram
-        </Link>
-      </div>
-    </section>
+        <div className="flex gap-3">
+          <Link
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href="#"
+          >
+            Lihat
+          </Link>
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.links.instagram}
+          >
+            <InstagramIcon size={20} />
+            Instagram
+          </Link>
+        </div>
+      </section>
+      {/* HERO::END */}
+      {/* KEGIATAN::START */}
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>Kegiatan</h1>
+        </div>
+
+        <div className="flex flex-wrap gap-3">
+          {siteConfig.kegiatans.map((kegiatan, i) => (
+            <Card
+              key={i}
+              isFooterBlurred
+              className="border-none aspect-square flex-[45%]"
+              radius="lg"
+            >
+              <Image
+                removeWrapper
+                alt="Kegiatan kepramukaan"
+                className="w-full h-full object-cover aspect-square"
+                src={kegiatan.image}
+              />
+              <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                <p className="text-tiny text-white/80">{kegiatan.name}</p>
+                <Link
+                  className={buttonStyles({
+                    color: "default",
+                    radius: "lg",
+                    variant: "flat",
+                    size: "sm",
+                    className: "text-tiny text-white bg-black/20",
+                  })}
+                  href="#"
+                >
+                  Lihat
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+
+        <div className="flex gap-3">
+          <Link
+            className={buttonStyles({
+              color: "default",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href="#"
+          >
+            Selengkapnya
+          </Link>
+        </div>
+      </section>
+      {/* KEGIATAN::END */}
+      {/* GALERI::START */}
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>Galeri</h1>
+        </div>
+
+        <div className="flex gap-3">
+          <Link
+            className={buttonStyles({
+              color: "default",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href="#"
+          >
+            Selengkapnya
+          </Link>
+        </div>
+      </section>
+      {/* GALERI::END */}
+    </>
   );
 }
--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,9 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Pramuka | Graha Pancaka",
+  description:
+    "Lihat kumpulan aktifitas dan galeri kami di gerakan pramuka graha pancaka. Bersatu, Berjuang, Menang!",
   navItems: [
     {
       label: "Home",
@@ -54,4 +55,18 @@ export const siteConfig = {
   links: {
     instagram: "https://www.instagram.com/pramukasmkn2bdg/",
   },
+  kegiatans: [
+    {
+      name: "Pelantikan",
+      image:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdarunnajah.com%2Fwp-content%2Fuploads%2F2021%2F03%2FWhatsApp-Image-2021-03-25-at-20.59.31.jpeg&f=1&nofb=1&ipt=f3cf54b47307e8ab86ff31701f08fe72c7430b6d31b208c47db889de84fb8f21&ipo=images",
+      link: "#",
+    },
+    {
+      name: "Lomba",
+      image:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FyYxK97ERAq0%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=daf092652366e9a83cad64dd0f076d30e2f49c1720ee0299d84cd5080240cc65&ipo=images",
+      link: "#",
+    },
+  ],
 };
