4d15bcba85d4aa6622c2fe05671b3afb4c8b875f
adnan-zahir <74122975+adnan-zahir@users.noreply.github.com>
2024-07-14 16:16:01 +0700

    INIT COMMIT

---
+++ config/site.ts
@@ -0,0 +1,57 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + NextUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Docs",
+      href: "/docs",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Beranda",
+      href: "/",
+    },
+    {
+      label: "Kegaitan",
+      href: "/kegiatan",
+    },
+    {
+      label: "Galeri",
+      href: "/galeri",
+    },
+    {
+      label: "Tentang Kami",
+      href: "/tentang",
+    },
+    {
+      label: "Kontak",
+      href: "/kontak",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    instagram: "https://www.instagram.com/pramukasmkn2bdg/",
+  },
+};
